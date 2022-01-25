f77ff7d4758d2bab0658b7e8074897737b57ff84
Hilmi Yilmaz <hilmiyilmaz.1998@gmail.com>
2022-01-25 15:18:38 +0100

    tokenizer done

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: hyilmaz <hyilmaz@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 17:53:31 by hyilmaz       #+#    #+#                 */
-/*   Updated: 2022/01/25 12:25:41 by hyilmaz       ########   odam.nl         */
+/*   Updated: 2022/01/25 15:15:37 by hyilmaz       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 ** Returns the amount of command tokens in a command.
 ** The commands tokens are all tokens except for the redirection operator
 ** and filename for redirection.
-*/
 
 static size_t	number_command_tokens(t_list *token_list)
 {
@@ -68,7 +67,6 @@ t_command	*create_simple_command_up_until_pipe_token(t_list *token_list, \
 			else
 				command->redirection_operator = HERE_DOC;
 			element = element->next;
-			/* Now at filename */
 			command->filename = ft_substr(((t_token *)(element->content))->content, 0, \
 										((t_token *)(element->content))->len_content);
 			*location += 2;
@@ -87,3 +85,5 @@ t_command	*create_simple_command_up_until_pipe_token(t_list *token_list, \
 		*location += 1;
 	return (command);
 }
+
+*/
