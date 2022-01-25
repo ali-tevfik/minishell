6c74d28cdb205aaf53e73157b165cb4b544dc0ec
ali-tevfik <tevfikdoner@hotmail.com>
2022-01-25 12:24:57 +0100

    master commands file

--- src/commands/env.c
+++ src/commands/env.c
@@ -1,28 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   commands.h                                         :+:    :+:            */
+/*   env.c                                              :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: adoner <adoner@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/01/18 15:56:45 by adoner        #+#    #+#                 */
-/*   Updated: 2022/01/25 12:16:52 by adoner        ########   odam.nl         */
+/*   Created: 2022/01/20 15:09:36 by adoner        #+#    #+#                 */
+/*   Updated: 2022/01/21 19:11:34 by adoner        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COMMANDS_H
-# define COMMANDS_H
+#include "../../incl/commands.h"
+#include "../../incl/minishell.h"
 
-# include "minishell.h"
-# include <errno.h>
-# define PATH_MAX 1024
+t_list	*add_envp(char *envp[])
+{
+	t_list	*envp_;
+	int		i;
 
-t_list	*add_envp(char *envp[]);
-void	cd_command(char *where);
-void	pwd_command(void);
-char	*skip_space(char *s);
-void	echo_command(char *text);
-int		skip_space_start(char *s);
-void	env_commands(t_list *envp);
+	i = 1;
+	envp_ = NULL;
+	envp_ = ft_lstnew(envp[0]);
+	while (envp[i])
+	{
+		ft_lstadd_back(&envp_, ft_lstnew(envp[i]));
+		i++;
+	}
+	return (envp_);
+}
 
-#endif
+void	env_commands(t_list *envp)
+{
+	while (envp != NULL)
+	{
+		printf("%s\n", (char *)envp->content);
+		envp = envp->next;
+	}
+}
